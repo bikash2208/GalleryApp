91709b42b63151d26d514a93049fd22c62b53d89
BIKASH <bikashchoudhury480@gmail.com>
2022-02-20 20:07:59 +0530

    fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'galleryapp-bikash.herokuapp.com/', protocol: 'https'}
+  config.action_mailer.default_url_options = {host: 'galleryapp-bikash.herokuapp.com/'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
