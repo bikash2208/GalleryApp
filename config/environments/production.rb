a6a680ff276275b03b9384cba6a9976961b06c73
BIKASH <bikashchoudhury480@gmail.com>
2022-02-20 19:34:50 +0530

    smtp implemented

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,21 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: 'galleryapp-bikash.herokuapp.com/'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.dig(:gmail_smtp, :email),
+    password:             Rails.application.credentials.dig(:gmail_smtp, :password),
+    authentication:       'plain',
+  }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
